66d95b198141bd62ae56adec8a35e136c25df201
Kevin Provance <kevin.provance@gmail.com>
2021-05-29 23:13:49 -0400

    Update .travis_push.sh

--- .travis_push.sh
+++ .travis_push.sh
@@ -20,10 +20,11 @@ commit_fonts_file() {
 }
 
 upload_files() {
+    
   # Remove existing "origin"
   git remote rm origin
   # Add new "origin" with access token in the git URL for authentication
-  git remote add origin https://dovy:${NEW_GH_TOKEN}@github.com/svl-studios/google-fonts.git > /dev/null 2>&1
+  git remote add origin https://kprovance:${GF_TOKEN}@github.com/svl-studios/google-fonts.git > /dev/null 2>&1
   git push origin master --quiet > /dev/null 2>&1
 }
 
