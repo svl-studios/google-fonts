6951382669f91ccc6b16b32b4e71fdbded14dd25
Kevin Provance <kevin.provance@gmail.com>
2021-06-11 14:04:07 -0400

    Update run.php

--- run.php
+++ run.php
@@ -1,5 +1,18 @@
 <?php
-
+function var_export_min( $var, $return = false ) {
+	if ( is_array( $var ) ) {
+		$toImplode = array();
+		foreach ( $var as $key => $value ) {
+			$toImplode[] = var_export( $key, true ) . '=>' . var_export_min( $value, true );
+		}
+		$code = 'array(' . implode( ',', $toImplode ) . ')';
+		if ( $return )
+			return $code; else echo $code;
+	} else {
+		return var_export( $var, $return );
+	}
+	return false;
+}
 
 /**
  * getSubsets Function.
@@ -94,6 +107,9 @@ date_default_timezone_set( 'UTC' );
 $output = shell_exec( 'git log -1' );
 echo shell_exec( 'git checkout -f master' );
 $gFile             = dirname( __FILE__ ) . '/google_fonts.json';
+$gFilePHP          = dirname( __FILE__ ) . '/fonts.php';
+$gFileminPHP       = dirname( __FILE__ ) . '/fonts-min.php';
+
 $fonts             = array();
 
 $arrContextOptions = array(
@@ -105,6 +121,7 @@ $arrContextOptions = array(
 
 $key               = $argv[1];
 $result            = json_decode( file_get_contents( "https://www.googleapis.com/webfonts/v1/webfonts?key={$key}", false, stream_context_create( $arrContextOptions ) ) );
+$cd                = date( 'Y-m-d h:i:s:a' );
 
 foreach ( $result->items as $font ) {
 	$fonts[ $font->family ] = array(
@@ -115,5 +132,15 @@ foreach ( $result->items as $font ) {
 
 ksort( $fonts );
 $data = json_encode( $fonts );
+
 echo "Saving JSON File\n\n";
-echo file_put_contents( $gFile, $data );
+file_put_contents( $gFile, $data );
+
+echo "Saving PHP\n\n";
+$code = <<<PHP
+<?php
+// Last Updated : $cd
+defined( 'ABSPATH' ) || exit; 
+return json_decode( $data, true );
+PHP;
+file_put_contents( $gFileminPHP, $code );
